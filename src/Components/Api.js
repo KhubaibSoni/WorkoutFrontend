f71a19c1dffadaf7f567dc4cb4fd99f9ac9ad403
Khubaib soni <153660915+KhubaibSoni@users.noreply.github.com>
2024-08-18 05:03:20 +0500

    Update Api.js

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -20,16 +20,12 @@ const ApiFunc = async (method = 'GET', headers = {}, body = null, url = '', id =
   } catch (error) {
     // Enhanced error handling
     if (error.response) {
-      console.error('Error response data:', error.response.data);
-      console.error('Error response status:', error.response.status);
       throw new Error(error.response.data.message || 'An error occurred');
     } else if (error.request) {
       // The request was made but no response received
-      console.error('No response received:', error.request);
       throw new Error('No response received from the server');
     } else {
       // Something happened in setting up the request
-      console.error('Error in API request setup:', error.message);
       throw new Error('Error in API request setup');
     }
   }
